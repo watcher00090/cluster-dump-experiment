fb22ba017d7842a810ea8340a23bb915afc3dc42
James Pedersen <james00090@gmail.com>
2021-05-26 18:12:08 -0700

    changes to trap handler

--- cluster-dump.sh
+++ cluster-dump.sh
@@ -12,9 +12,7 @@ done
 kubectl proxy --port=${PORT} &
 
 # Trap handler to clean up child process if script is ^C ed
-#trap 'echo "Exiting the script..."; kill -9 $!; exit 0' SIGINT SIGTERM
-
-trap 'kill $!; if [ -z "$(kill -0 $!)" ]; then kill -9 $!; fi; exit 0' SIGINT SIGTERM
+trap 'kill $!; wait $!; exit 0' SIGINT SIGTERM
 
 sleep 10
  
@@ -29,10 +27,9 @@ namespaces=($(curl -s -X GET $APISERVER/api/v1/namespaces | jq '.items[].metadat
 echo ${namespaces[@]}
 
 echo "sleeping..."
-sleep 40
+sleep 10
 
-if [ -z "$(kill -0 $!)" ]; then kill $!; fi # kill -0 $CHILD_PID returned nothing so the child is still running
-if [ -z "$(kill -0 $!)" ]; then kill -9 $!; fi # The child is still running, force kill it
+if [ -z "$(kill -0 $! 2>&1)" ]; then kill $!; wait $!; fi # kill -0 $CHILD_PID returned nothing so the child is still running
 
 # For all global resources, run kubectl describe
 

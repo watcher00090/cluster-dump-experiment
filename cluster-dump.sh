e8fd9e99098506d2f60ad23fff21278ebe394ea6
James Pedersen <james00090@gmail.com>
2021-06-08 13:20:08 -0700

    cluster dump running kubectl describe on all global resources

--- cluster-dump.sh
+++ cluster-dump.sh
@@ -24,14 +24,14 @@ trap 'kill $!; wait $!; exit 0' SIGINT SIGTERM
  
 APISERVER=localhost:${PORT}
 
-function compute_cumulative() {
-    ARR=("$@"); RET=(); tot=0;
-    for k in ${!ARR[@]}; do
-        tot=$(($tot+${ARR[$k]}))
-        RET+=(${tot})
-    done
-    echo "${RET[@]}"
-}
+# function compute_cumulative() {
+#     ARR=("$@"); RET=(); tot=0;
+#     for k in ${!ARR[@]}; do
+#         tot=$(($tot+${ARR[$k]}))
+#         RET+=(${tot})
+#     done
+#     echo "${RET[@]}"
+# }
 
 contains_element () {
   local e match="$1"
@@ -106,15 +106,43 @@ api_group() {
     _given_idx="$1"; shift 
     RESOURCE_COUNT=("$@");
     _tot=0; 
+
+    # echo "_given_idx=$_given_idx"
+    # echo "RESOURCE_COUNT = ${RESOURCE_COUNT[@]}"
     
     for _idx in ${!RESOURCE_COUNT[@]}; do
+        # echo "tot=$_tot"
         _tot=$(($_tot+${RESOURCE_COUNT[$_idx]}))
         if [ $_given_idx -le $(($_tot-1)) ]; then 
-            _EXITED_LOOP=1
-            break;
+            echo $_idx
+            return 0;
         fi
     done
-    if [ ! $_EXITED_LOOP ]; then echo -1; fi
+
+    echo -1; return 1;
+}
+
+sift() {
+    local versions_arr
+    local ret
+    
+    versions_arr=("$@"); shift
+
+    if [ ${#versions_arr[@]} == 3 ]; then
+        echo "v1alpha1 v1beta1 v1"; return 0
+    elif [ ${#versions_arr[@]} == 2 ]; then
+        if [ ${versions_arr[0]} == "v1" ] || [ ${versions_arr[1]} == "v1" ]; then
+            if [ "${versions_arr[0]}" == "v1alpha1" ] || [ "${versions_arr[1]}" == "v1alpha1" ]; then echo "v1alpha1 v1"; return 0; fi
+            if [ "${versions_arr[0]}" == "v1beta1" ] || [ "${versions_arr[1]}" == "v1beta1" ]; then echo "v1beta1 v1"; return 0; fi
+        else echo "v1alpha1 v1beta1"; fi
+    elif [ ${#versions_arr[@]} == 1 ]; then
+        echo "${versions_arr[@]}"; return 0
+    elif [ ${#versions_arr[@]} == 0 ]; then
+        echo ""; return 0
+    else
+        echo "ERROR: unrecognized version array size in sift()"
+        return -1;    
+    fi
 }
 
 # Get and store all api groups in an array
@@ -158,7 +186,7 @@ for k in ${!api_groups[@]}; do
         if [ "$RET" == "0" ]; then 
             # echo herev1alpha1
 
-            echo "api_versions_for_group = ${api_versions_for_group[@]}"
+            # echo "api_versions_for_group = ${api_versions_for_group[@]}"
 
             non_namespaced_resource_types_in_group+=($(curl -s $STARTER_PATH/v1alpha1 | jq '.resources[] | select(.namespaced==false) | .name' | tr -d \" | tr -d \') )
             namespaced_resource_types_in_group+=($(curl -s $STARTER_PATH/v1alpha1 | jq '.resources[] | select(.namespaced==true) | .name' | tr -d \" | tr -d \') )
@@ -183,21 +211,21 @@ for k in ${!api_groups[@]}; do
             # echo therev1
         fi
 
-        echo "group_name = ${group_name}"
-        echo "starter_path = $STARTER_PATH"
-        echo "sz before=${#namespaced_resource_types_in_group[@]}"
+        # echo "group_name = ${group_name}"
+        # echo "starter_path = $STARTER_PATH"
+        # echo "sz before=${#namespaced_resource_types_in_group[@]}"
 
         non_namespaced_resource_types_in_group=($(remove_duplicates "${non_namespaced_resource_types_in_group[@]}"))
         namespaced_resource_types_in_group=($(remove_duplicates "${namespaced_resource_types_in_group[@]}"))
 
-        echo "sz after=${#namespaced_resource_types_in_group[@]}"
+        # echo "sz after=${#namespaced_resource_types_in_group[@]}"
     fi
 
     # For figuring out the API group of a given resource type, later
     sz_non_namespaced=${#non_namespaced_resource_types_in_group[@]}
     sz_namespaced=${#namespaced_resource_types_in_group[@]}
-    num_non_namespaced_resource_types_per_api_group=("${num_non_namespaced_resource_types_per_api_group[@]}" "${sz_non_namespaced}")
-    num_namespaced_resource_types_per_api_group=("${num_namespaced_resource_types_per_api_group[@]}" "${sz_namespaced}")
+    num_non_namespaced_resource_types=("${num_non_namespaced_resource_types[@]}" "${sz_non_namespaced}")
+    num_namespaced_resource_types=("${num_namespaced_resource_types[@]}" "${sz_namespaced}")
 
     non_namespaced_resource_types+=("${non_namespaced_resource_types_in_group[@]}")
     namespaced_resource_types+=("${namespaced_resource_types_in_group[@]}")
@@ -205,22 +233,90 @@ for k in ${!api_groups[@]}; do
     # echo "namespaced_resource_types = ${namespaced_resource_types[@]}"
 done
 
-echo "num_namespaced_resource_types_per_api_group = ${num_namespaced_resource_types_per_api_group[@]}"
-echo "num_non_namespaced_resource_types_per_api_group = ${num_namespaced_resource_types_per_api_group[@]}"
+# echo "num_namespaced_resource_types_per_api_group = ${num_namespaced_resource_types_per_api_group[@]}"
+# echo "num_non_namespaced_resource_types_per_api_group = ${num_namespaced_resource_types_per_api_group[@]}"
 
-echo "namespaced_resource_types = ${namespaced_resource_types[@]}"
-echo
-echo "non_namespaced_resource_types = ${non_namespaced_resource_types[@]}"
+# echo "namespaced_resource_types = ${namespaced_resource_types[@]}"
+# echo
+# echo "non_namespaced_resource_types = ${non_namespaced_resource_types[@]}"
+
+printf "$ kubectl version\n"
+kubectl version
 
 printf "############################################################\n"
 printf "######################GLOBAL RESOURCES######################\n"
-printf "############################################################\n"
+printf "############################################################\n\n"
 
+for idx in ${!non_namespaced_resource_types[@]}; do
 
+    resource_type=
+    api_group_idx=
+    api_group=
+    STARTER_PATH=
+    api_versions=
+    belongs_to_group_version_k=
+    objects=
 
+    resource_type=${non_namespaced_resource_types[$idx]}
+    api_group_idx=$(api_group $idx ${num_non_namespaced_resource_types[@]})
+    echo "api_group_idx=$api_group_idx"
+    api_group=${api_groups[$api_group_idx]}
+    
+    if [ "$api_group" == "core" ]; then
+        STARTER_PATH="$APISERVER/api"
+        api_versions=("v1")
+    else
+        STARTER_PATH="$APISERVER/apis/$api_group"
+        api_versions=($(curl -s $STARTER_PATH | jq '.versions[].version' | tr -d \' | tr -d \"))
+    fi
+
+    # order the api_versions array so that 'v1alpha1' comes before 'v1beta1' comes before 'v1'
+
+    # echo "api_versions_before = ${api_versions[@]}"
+    sift "${api_versions[@]}"
+
+    api_versions=($(sift "${api_versions[@]}"))
+    belongs_to_group_version_k=() # array the same size as the api_versions array, 
+
+    # echo "api_versions_after = ${api_versions[@]}"
+
+    for k in ${!api_versions[@]}; do 
+        belongs_to_group_version_k+=("0")
+    done
+
+    for k in ${!api_versions[@]}; do 
+        status=$(curl -s $STARTER_PATH/${api_versions[$k]}/$resource_type | jq '.status' | tr -d \" | tr -d \')
+        if [ "$status" == "Failure" ]; then belongs_to_group_version_k[$k]=0;
+        elif [ "$status" == "null" ]; then belongs_to_group_version_k[$k]=1;
+        else echo "Unrecognized status received, exiting..."; exit 1; fi
+    done
+
+    # echo "belongs_to_group_version_k = ${belongs_to_group_version_k[@]}"
+    # echo "api_group = ${api_group}"
+    # echo "api_versions = ${api_versions[@]}"
+    # echo "belongs_to_group_version_k = ${belongs_to_group_version_k[@]}"
+
+    for k in ${!belongs_to_group_version_k[@]}; do
+        if [ "${belongs_to_group_version_k[$k]}" == "1" ]; then
+            printf "$resource_type (API group: $api_group)\n:"
+            printf "Summary:\n"
+            printf "$ kubectl get -A $resource_type\n"
+            kubectl get -A ${resource_type}
+
+            objects=($(curl -s $STARTER_PATH/${api_versions[$k]}/$resource_type | jq '.items[].metadata.name' | tr -d \' | tr -d \"))
+            for name in ${objects[@]}; do
+                printf "${name}:"
+                printf "$ kubectl describe ${resource_type}/${name}\n"
+                kubectl describe ${resource_type} ${name}
+            done
+        fi
+    done    
+done
+
+printf "\n"
 printf "############################################################\n"
 printf "####################NAMESPACED RESOURCES####################\n"
-printf "############################################################\n"
+printf "############################################################\n\n"
 
 
 if [ -z "$(kill -0 $! 2>&1)" ]; then kill $!; wait $!; fi # kill -0 $CHILD_PID returned nothing so the child is still running
